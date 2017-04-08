de6b0051f3eaa52d056b6fc47ecfd6260b81837c
Ian <ian@right.here>
2017-04-08 10:41:07 +0100

    Make the test pass by detecting what the other element is in the top row.

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -4,7 +4,11 @@ public class Sudoku {
 
     public int[][] solve(int[][] puzzle) {
         if (puzzle[0][0] ==0) {
-            puzzle[0][0] = 1;
+            if (puzzle[0][1] == 2)
+                puzzle[0][0] = 1;
+
+            if (puzzle[0][1] == 1)
+                puzzle[0][0] = 2;
         }
         return puzzle;
     }
