0872dd8b73d6c0218f75ac68d86a7ae12562bbbe
Ian <ian@right.here>
2017-04-08 11:18:49 +0100

    The next failing test is with a zero in the bottom right and a complementary soltuion

--- test/sudoku/SudokuTest.java
+++ test/sudoku/SudokuTest.java
@@ -45,7 +45,10 @@ public class SudokuTest {
 
                 dynamicTest("test", () -> sudokuTest(
                         "1 2",  "1 2",
-                        "2 0",  "2 1"))
+                        "2 0",  "2 1")),
+                dynamicTest("test", () -> sudokuTest(
+                        "2 1",  "2 1",
+                        "1 0",  "1 2"))
         );
     }
 
