86988e766d351cb019b47f52e8faed725b793d87
Ian <ian@right.here>
2017-04-08 12:03:45 +0100

    Make the tests pass again by making recursive calls to be able to access code that has moved up in the solver.

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -22,11 +22,15 @@ public class Sudoku {
         }
 
         if (puzzle[0][1] == 0) {
-            if (puzzle[1][1] == 1)
+            if (puzzle[1][1] == 1) {
                 puzzle[0][1] = 2;
+                return solve(puzzle);
+            }
 
-            if (puzzle[1][1] == 2)
+            if (puzzle[1][1] == 2) {
                 puzzle[0][1] = 1;
+                return solve(puzzle);
+            }
 
             if (puzzle[0][0] == 1)
                 puzzle[0][1] = 2;
