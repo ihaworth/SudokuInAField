f3e56e96b4e58d496b80c037ccce629fd5bdbad1
Ian <ian@right.here>
2017-04-08 11:21:26 +0100

    The next failing test has zeros in the bottom row

--- test/sudoku/SudokuTest.java
+++ test/sudoku/SudokuTest.java
@@ -48,7 +48,11 @@ public class SudokuTest {
                         "2 0",  "2 1")),
                 dynamicTest("test", () -> sudokuTest(
                         "2 1",  "2 1",
-                        "1 0",  "1 2"))
+                        "1 0",  "1 2")),
+
+                dynamicTest("test", () -> sudokuTest(
+                        "1 2",  "1 2",
+                        "0 0",  "2 1"))
         );
     }
 
