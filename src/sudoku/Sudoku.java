e599686431938d860391dbcd274713d130751ed8
Ian <ian@right.here>
2017-04-08 12:10:53 +0100

    Move the case for the zero top right to the end to flush out more failures due to ordering.

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -21,24 +21,6 @@ public class Sudoku {
             }
         }
 
-        if (puzzle[0][1] == 0) {
-            if (puzzle[1][1] == 1) {
-                puzzle[0][1] = 2;
-                return solve(puzzle);
-            }
-
-            if (puzzle[1][1] == 2) {
-                puzzle[0][1] = 1;
-                return solve(puzzle);
-            }
-
-            if (puzzle[0][0] == 1)
-                puzzle[0][1] = 2;
-
-            if (puzzle[0][0] == 2)
-                puzzle[0][1] = 1;
-        }
-
         if (puzzle[1][0] == 0) {
             if (puzzle[1][1] == 1)
                 puzzle[1][0] = 2;
@@ -57,6 +39,24 @@ public class Sudoku {
                 puzzle[1][1] = 2;
                 return solve(puzzle);
             }
+        }
+
+        if (puzzle[0][1] == 0) {
+            if (puzzle[1][1] == 1) {
+                puzzle[0][1] = 2;
+                return solve(puzzle);
+            }
+
+            if (puzzle[1][1] == 2) {
+                puzzle[0][1] = 1;
+                return solve(puzzle);
+            }
+
+            if (puzzle[0][0] == 1)
+                puzzle[0][1] = 2;
+
+            if (puzzle[0][0] == 2)
+                puzzle[0][1] = 1;
         }
 
         return puzzle;
