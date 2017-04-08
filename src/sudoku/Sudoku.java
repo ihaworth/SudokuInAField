12deccdae72a69fc8da636e031f1a3aa12a0a522
Ian <ian@right.here>
2017-04-08 10:47:01 +0100

    Make the test pass with a top-level test for top right.  Note that this falls through to the following case to fix the zero top left.

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -3,7 +3,10 @@ package sudoku;
 public class Sudoku {
 
     public int[][] solve(int[][] puzzle) {
-        if (puzzle[0][0] ==0) {
+        if (puzzle[0][1] == 0) {
+            puzzle[0][1] = 2;
+        }
+        if (puzzle[0][0] == 0) {
             if (puzzle[0][1] == 2)
                 puzzle[0][0] = 1;
 
