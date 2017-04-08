12cf270ee2f48b244c102367df8847c32f0af91f
Ian <ian@right.here>
2017-04-08 12:13:46 +0100

    Add recursive calls to all cases as this pattern is now well established

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -4,11 +4,15 @@ public class Sudoku {
 
     public int[][] solve(int[][] puzzle) {
         if (puzzle[0][0] == 0) {
-            if (puzzle[0][1] == 2)
+            if (puzzle[0][1] == 2) {
                 puzzle[0][0] = 1;
+                return solve(puzzle);
+            }
 
-            if (puzzle[0][1] == 1)
+            if (puzzle[0][1] == 1) {
                 puzzle[0][0] = 2;
+                return solve(puzzle);
+            }
 
             if (puzzle[1][0] == 2) {
                 puzzle[0][0] = 1;
@@ -22,11 +26,15 @@ public class Sudoku {
         }
 
         if (puzzle[1][0] == 0) {
-            if (puzzle[1][1] == 1)
+            if (puzzle[1][1] == 1) {
                 puzzle[1][0] = 2;
+                return solve(puzzle);
+            }
 
-            if (puzzle[1][1] == 2)
+            if (puzzle[1][1] == 2) {
                 puzzle[1][0] = 1;
+                return solve(puzzle);
+            }
         }
 
         if (puzzle[1][1] == 0) {
