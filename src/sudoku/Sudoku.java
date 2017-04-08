856760302c46bf11d209c147900155a0d290da6a
Ian <ian@right.here>
2017-04-08 12:19:03 +0100

    Fill out the zero bottom left to be able to inspect rows aswell as columns

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -35,6 +35,16 @@ public class Sudoku {
                 puzzle[1][0] = 1;
                 return solve(puzzle);
             }
+
+            if (puzzle[0][0] == 1) {
+                puzzle[1][0] = 2;
+                return solve(puzzle);
+            }
+
+            if (puzzle[0][0] == 2) {
+                puzzle[1][0] = 1;
+                return solve(puzzle);
+            }
         }
 
         if (puzzle[1][1] == 0) {
