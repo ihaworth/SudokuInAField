a867b762e6d469679c02a96330108049dbc8fb58
Ian <ian@right.here>
2017-04-08 11:22:57 +0100

    Since the solver is already solving the puzzle for the bottom right zero, simply re-order the solver so that it can then use this info to solve the remaining bottom left zero

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -19,20 +19,20 @@ public class Sudoku {
                 puzzle[0][0] = 2;
         }
 
-        if (puzzle[1][0] == 0) {
-            if (puzzle[1][1] == 1)
-                puzzle[1][0] = 2;
-
-            if (puzzle[1][1] == 2)
-                puzzle[1][0] = 1;
-        }
-
         if (puzzle[1][1] == 0) {
             if (puzzle[0][1] == 2)
                 puzzle[1][1] = 1;
 
             if (puzzle[0][1] == 1)
                 puzzle[1][1] = 2;
+        }
+
+        if (puzzle[1][0] == 0) {
+            if (puzzle[1][1] == 1)
+                puzzle[1][0] = 2;
+
+            if (puzzle[1][1] == 2)
+                puzzle[1][0] = 1;
         }
 
         return puzzle;
