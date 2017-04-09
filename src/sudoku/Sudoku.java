442c65a43c027b41f6d4199846540bbd78318b99
Ian <ian@right.here>
2017-04-09 10:22:10 +0100

    Make the code more similar by introducing the otherNumber() method that decides what to set the cell to

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -8,22 +8,22 @@ public class Sudoku {
             int col = 0;
             if (puzzle[row][col] == 0) {
                 if (puzzle[row][1] == 2) {
-                    puzzle[row][col] = 1;
+                    puzzle[row][col] = otherNumber(puzzle[row][1]);
                     return solve(puzzle);
                 }
 
                 if (puzzle[row][1] == 1) {
-                    puzzle[row][col] = 2;
+                    puzzle[row][col] = otherNumber(puzzle[row][1]);
                     return solve(puzzle);
                 }
 
                 if (puzzle[1][col] == 2) {
-                    puzzle[row][col] = 1;
+                    puzzle[row][col] = otherNumber(puzzle[1][col]);
                     return solve(puzzle);
                 }
 
                 if (puzzle[1][col] == 1) {
-                    puzzle[row][col] = 2;
+                    puzzle[row][col] = otherNumber(puzzle[1][col]);
                     return solve(puzzle);
                 }
             }
@@ -33,23 +33,23 @@ public class Sudoku {
             int row = 1;
             int col = 0;
             if (puzzle[row][col] == 0) {
-                if (puzzle[row][1] == 1) {
-                    puzzle[row][col] = 2;
-                    return solve(puzzle);
-                }
-
                 if (puzzle[row][1] == 2) {
-                    puzzle[row][col] = 1;
+                    puzzle[row][col] = otherNumber(puzzle[row][1]);
                     return solve(puzzle);
                 }
 
-                if (puzzle[0][col] == 1) {
-                    puzzle[row][col] = 2;
+                if (puzzle[row][1] == 1) {
+                    puzzle[row][col] = otherNumber(puzzle[row][1]);
                     return solve(puzzle);
                 }
 
                 if (puzzle[0][col] == 2) {
-                    puzzle[row][col] = 1;
+                    puzzle[row][col] = otherNumber(puzzle[0][col]);
+                    return solve(puzzle);
+                }
+
+                if (puzzle[0][col] == 1) {
+                    puzzle[row][col] = otherNumber(puzzle[0][col]);
                     return solve(puzzle);
                 }
             }
@@ -59,23 +59,23 @@ public class Sudoku {
             int row = 1;
             int col = 1;
             if (puzzle[row][col] == 0) {
-                if (puzzle[0][col] == 2) {
-                    puzzle[row][col] = 1;
-                    return solve(puzzle);
-                }
-
-                if (puzzle[0][col] == 1) {
-                    puzzle[row][col] = 2;
-                    return solve(puzzle);
-                }
-
                 if (puzzle[row][0] == 2) {
-                    puzzle[row][col] = 1;
+                    puzzle[row][col] = otherNumber(puzzle[row][0]);
                     return solve(puzzle);
                 }
 
                 if (puzzle[row][0] == 1) {
-                    puzzle[row][col] = 2;
+                    puzzle[row][col] = otherNumber(puzzle[row][0]);
+                    return solve(puzzle);
+                }
+
+                if (puzzle[0][col] == 2) {
+                    puzzle[row][col] = otherNumber(puzzle[0][col]);
+                    return solve(puzzle);
+                }
+
+                if (puzzle[0][col] == 1) {
+                    puzzle[row][col] = otherNumber(puzzle[0][col]);
                     return solve(puzzle);
                 }
             }
@@ -85,29 +85,33 @@ public class Sudoku {
             int row = 0;
             int col = 1;
             if (puzzle[row][col] == 0) {
-                if (puzzle[1][col] == 1) {
-                    puzzle[row][col] = 2;
-                    return solve(puzzle);
-                }
-
-                if (puzzle[1][col] == 2) {
-                    puzzle[row][col] = 1;
+                if (puzzle[row][0] == 2) {
+                    puzzle[row][col] = otherNumber(puzzle[row][0]);
                     return solve(puzzle);
                 }
 
                 if (puzzle[row][0] == 1) {
-                    puzzle[row][col] = 2;
+                    puzzle[row][col] = otherNumber(puzzle[row][0]);
                     return solve(puzzle);
                 }
 
-                if (puzzle[row][0] == 2) {
-                    puzzle[row][col] = 1;
+                if (puzzle[1][col] == 2) {
+                    puzzle[row][col] = otherNumber(puzzle[1][col]);
+                    return solve(puzzle);
+                }
+
+                if (puzzle[1][col] == 1) {
+                    puzzle[row][col] = otherNumber(puzzle[1][col]);
                     return solve(puzzle);
                 }
             }
         }
 
         return puzzle;
+    }
+
+    private int otherNumber(int i) {
+        return (i == 1) ? 2 : 1;
     }
 
 }
