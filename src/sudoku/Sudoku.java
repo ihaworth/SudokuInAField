0099263c03c3474202b57e42421f56410d9be9bc
Ian <ian@right.here>
2017-04-09 10:09:01 +0100

    Extract row and col variables for each case

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -3,91 +3,107 @@ package sudoku;
 public class Sudoku {
 
     public int[][] solve(int[][] puzzle) {
-        if (puzzle[0][0] == 0) {
-            if (puzzle[0][1] == 2) {
-                puzzle[0][0] = 1;
-                return solve(puzzle);
-            }
+        {
+            int row = 0;
+            int col = 0;
+            if (puzzle[row][col] == 0) {
+                if (puzzle[row][1] == 2) {
+                    puzzle[row][col] = 1;
+                    return solve(puzzle);
+                }
 
-            if (puzzle[0][1] == 1) {
-                puzzle[0][0] = 2;
-                return solve(puzzle);
-            }
+                if (puzzle[row][1] == 1) {
+                    puzzle[row][col] = 2;
+                    return solve(puzzle);
+                }
 
-            if (puzzle[1][0] == 2) {
-                puzzle[0][0] = 1;
-                return solve(puzzle);
-            }
+                if (puzzle[1][col] == 2) {
+                    puzzle[row][col] = 1;
+                    return solve(puzzle);
+                }
 
-            if (puzzle[1][0] == 1) {
-                puzzle[0][0] = 2;
-                return solve(puzzle);
-            }
-        }
-
-        if (puzzle[1][0] == 0) {
-            if (puzzle[1][1] == 1) {
-                puzzle[1][0] = 2;
-                return solve(puzzle);
-            }
-
-            if (puzzle[1][1] == 2) {
-                puzzle[1][0] = 1;
-                return solve(puzzle);
-            }
-
-            if (puzzle[0][0] == 1) {
-                puzzle[1][0] = 2;
-                return solve(puzzle);
-            }
-
-            if (puzzle[0][0] == 2) {
-                puzzle[1][0] = 1;
-                return solve(puzzle);
+                if (puzzle[1][col] == 1) {
+                    puzzle[row][col] = 2;
+                    return solve(puzzle);
+                }
             }
         }
 
-        if (puzzle[1][1] == 0) {
-            if (puzzle[0][1] == 2) {
-                puzzle[1][1] = 1;
-                return solve(puzzle);
-            }
+        {
+            int row = 1;
+            int col = 0;
+            if (puzzle[row][col] == 0) {
+                if (puzzle[row][1] == 1) {
+                    puzzle[row][col] = 2;
+                    return solve(puzzle);
+                }
 
-            if (puzzle[0][1] == 1) {
-                puzzle[1][1] = 2;
-                return solve(puzzle);
-            }
+                if (puzzle[row][1] == 2) {
+                    puzzle[row][col] = 1;
+                    return solve(puzzle);
+                }
 
-            if (puzzle[1][0] == 2) {
-                puzzle[1][1] = 1;
-                return solve(puzzle);
-            }
+                if (puzzle[0][col] == 1) {
+                    puzzle[row][col] = 2;
+                    return solve(puzzle);
+                }
 
-            if (puzzle[1][0] == 1) {
-                puzzle[1][1] = 2;
-                return solve(puzzle);
+                if (puzzle[0][col] == 2) {
+                    puzzle[row][col] = 1;
+                    return solve(puzzle);
+                }
             }
         }
 
-        if (puzzle[0][1] == 0) {
-            if (puzzle[1][1] == 1) {
-                puzzle[0][1] = 2;
-                return solve(puzzle);
-            }
+        {
+            int row = 1;
+            int col = 1;
+            if (puzzle[row][col] == 0) {
+                if (puzzle[0][col] == 2) {
+                    puzzle[row][col] = 1;
+                    return solve(puzzle);
+                }
 
-            if (puzzle[1][1] == 2) {
-                puzzle[0][1] = 1;
-                return solve(puzzle);
-            }
+                if (puzzle[0][col] == 1) {
+                    puzzle[row][col] = 2;
+                    return solve(puzzle);
+                }
 
-            if (puzzle[0][0] == 1) {
-                puzzle[0][1] = 2;
-                return solve(puzzle);
-            }
+                if (puzzle[row][0] == 2) {
+                    puzzle[row][col] = 1;
+                    return solve(puzzle);
+                }
 
-            if (puzzle[0][0] == 2) {
-                puzzle[0][1] = 1;
-                return solve(puzzle);
+                if (puzzle[row][0] == 1) {
+                    puzzle[row][col] = 2;
+                    return solve(puzzle);
+                }
+            }
+        }
+
+        {
+            int row = 0;
+            int col = 1;
+            if (puzzle[row][col] == 0) {
+                if (puzzle[1][col] == 1) {
+                    puzzle[row][col] = 2;
+                    return solve(puzzle);
+                }
+
+                if (puzzle[1][col] == 2) {
+                    puzzle[row][col] = 1;
+                    return solve(puzzle);
+                }
+
+                if (puzzle[row][0] == 1) {
+                    puzzle[row][col] = 2;
+                    return solve(puzzle);
+                }
+
+                if (puzzle[row][0] == 2) {
+                    puzzle[row][col] = 1;
+                    return solve(puzzle);
+                }
             }
         }
 
