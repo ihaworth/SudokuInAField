e3320ed43089305705d6175c8db483db5ccaf14f
Ian <ian@right.here>
2017-04-09 11:25:58 +0100

    Extract the SudokyTest class from the Sudoku2By2Test class to make the test helper code shareable

--- test/sudoku/Sudoku2By2Test.java
+++ test/sudoku/Sudoku2By2Test.java
@@ -11,9 +11,7 @@ import static matchers.GridMatcher.gridMatches;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
-public class Sudoku2By2Test {
-
-    private final Sudoku sudoku = new Sudoku();
+public class Sudoku2By2Test extends SudokuTest {
 
     @TestFactory
     List<DynamicTest> sudokuTests() {
@@ -51,45 +49,6 @@ public class Sudoku2By2Test {
                         "0 0",  "2 1",
                         "0 2",  "1 2"))
         );
-    }
-
-    private void sudokuTest(String... rows) {
-
-        int [][] puzzle   = buildGrid(even(rows));
-        int [][] solution = buildGrid(odd(rows));
-
-        assertThat(sudoku.solve(puzzle), gridMatches(solution));
-    }
-
-    private String[] even(String[] rows) {
-        return oddOrEvenRows(rows, 0);
-    }
-
-    private String[] odd(String[] rows) {
-        return oddOrEvenRows(rows, 1);
-    }
-
-    private String[] oddOrEvenRows(String[] allRows, int oddOrEven) {
-
-        assert(allRows.length % 2 == 0);
-
-        String[] rows = new String[allRows.length / 2];
-
-        for (int i = 0; i < allRows.length; i++)
-            if (i % 2 == oddOrEven)
-                rows[i / 2] = allRows[i];
-
-        return rows;
-    }
-
-    private int[][] buildGrid(String[] rows) {
-        int[][] grid = new int[rows.length][rows.length];
-        for (int i = 0; i < rows.length; i++) {
-            String[] elements = rows[i].split(" ");
-            for (int j = 0; j < elements.length; j++)
-                grid[i][j] = parseInt(elements[j]);
-        }
-        return grid;
     }
 
 }
