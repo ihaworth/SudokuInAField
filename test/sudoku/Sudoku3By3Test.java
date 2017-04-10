561dad3995053c7eee54ba3e2f70b634a69a2a56
Ian <ian@right.here>
2017-04-10 10:24:11 +0100

    Add a passing test for missing a 2 from an inner 2x2

--- test/sudoku/Sudoku3By3Test.java
+++ test/sudoku/Sudoku3By3Test.java
@@ -16,6 +16,11 @@ public class Sudoku3By3Test extends SudokuTest {
                 dynamicTest("Identity",() -> sudokuTest(
                         "1 2 3",  "1 2 3",
                         "2 3 1",  "2 3 1",
+                        "3 1 2",  "3 1 2")),
+
+                dynamicTest("Missing a 2 from an inner 2x2",() -> sudokuTest(
+                        "1 0 3",  "1 2 3",
+                        "2 3 1",  "2 3 1",
                         "3 1 2",  "3 1 2"))
         );
     }
