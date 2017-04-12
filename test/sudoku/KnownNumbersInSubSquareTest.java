020f40ecba5fab5288554bae65e5b9e01715e239
Ian <ian@right.here>
2017-04-12 12:40:23 +0100

    Test that a 3x3 has no sub-squares

--- test/sudoku/KnownNumbersInSubSquareTest.java
+++ test/sudoku/KnownNumbersInSubSquareTest.java
@@ -14,12 +14,32 @@ public class KnownNumbersInSubSquareTest {
     public void a2x2PuzzleHasNoSubSquares() {
 
         puzzle = new int[][] {{1, 2},
-                              {2, 1}};
+                {2, 1}};
 
         assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 0, 0).isEmpty());
         assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 0, 1).isEmpty());
         assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 1, 0).isEmpty());
         assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 1, 1).isEmpty());
+    }
+
+    @Test
+    public void a3x3PuzzleHasNoSubSquares() {
+
+        puzzle = new int[][] {{1, 2, 3},
+                              {2, 3, 1},
+                              {3, 1, 2}};
+
+        assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 0, 0).isEmpty());
+        assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 0, 1).isEmpty());
+        assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 0, 2).isEmpty());
+
+        assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 1, 0).isEmpty());
+        assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 1, 1).isEmpty());
+        assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 1, 2).isEmpty());
+
+        assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 2, 0).isEmpty());
+        assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 2, 1).isEmpty());
+        assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 2, 2).isEmpty());
     }
 
 }
