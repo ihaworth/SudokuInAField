75d23503616a07cbde761fcc94d46fbd7dc38198
Ian <ian@right.here>
2017-04-12 12:08:51 +0100

    Add a test for another minimal puzzle to close out the cases for 3x3.  It already passes.

--- test/sudoku/Sudoku3By3Test.java
+++ test/sudoku/Sudoku3By3Test.java
@@ -36,7 +36,12 @@ public class Sudoku3By3Test extends SudokuTest {
                 dynamicTest("A puzzle where other cells need to be solved before other cells can be completed",() -> sudokuTest(
                         "1 0 0",  "1 3 2",
                         "0 0 3",  "2 1 3",
-                        "0 0 0",  "3 2 1"))
+                        "0 0 0",  "3 2 1")),
+
+                dynamicTest("Another minimal puzzle",() -> sudokuTest(
+                        "0 0 0",  "1 3 2",
+                        "0 0 3",  "2 1 3",
+                        "0 2 0",  "3 2 1"))
         );
     }
 
