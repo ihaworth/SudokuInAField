be6da453f9e73ed28f1eb2290ae02f3adae2816c
Ian <ian@right.here>
2017-04-12 13:19:27 +0100

    Add a test for a top right sub-square

--- test/sudoku/KnownNumbersInSubSquareTest.java
+++ test/sudoku/KnownNumbersInSubSquareTest.java
@@ -89,6 +89,20 @@ public class KnownNumbersInSubSquareTest {
         assertTrue(sudoku.knownNumbersInSubSquare(puzzle, 1, 1).isEmpty());
     }
 
+    @Test
+    public void a4x4PuzzleHasATopRightSubSquare() {
+
+        puzzle = new int[][] {{0, 0, 3, 2},
+                              {0, 0, 1, 4},
+                              {0, 0, 0, 0},
+                              {0, 0, 0, 0}};
+
+        assertEquals(sudoku.knownNumbersInSubSquare(puzzle, 0, 2), set(1, 2, 3, 4));
+        assertEquals(sudoku.knownNumbersInSubSquare(puzzle, 0, 3), set(1, 2, 3, 4));
+        assertEquals(sudoku.knownNumbersInSubSquare(puzzle, 1, 2), set(1, 2, 3, 4));
+        assertEquals(sudoku.knownNumbersInSubSquare(puzzle, 1, 3), set(1, 2, 3, 4));
+    }
+
     private Set<Integer> set(Integer... ints) {
         return new HashSet<>(Arrays.asList(ints));
     }
